837d15a2ea9ddd95864cdd10e561c4592371dac9
moonwwdz <wangy3483@gmail.com>
2018-10-29 08:46:39 -0400

    get last update fiction

--- content/content.go
+++ content/content.go
@@ -15,6 +15,32 @@ type cont struct {
 	Url     string
 }
 
+// GetLastTitleList 获取最近更新列表
+func GetLastTitleList(num int) []cont {
+	url := "https://m.biqiuge.com/"
+	var ret []cont
+	doc := get(url + "book_4772/")
+	doc.Find(".books .listpage select option").Each(func(i int, s *goquery.Selection) {
+		values, _ := s.Attr("value")
+		c := cont{Url: values}
+		ret = append(ret, c)
+	})
+
+	//取最后一页上的所有章节名
+	lastC := ret[len(ret)-1]
+	titleListDoc := get(url + lastC.Url)
+	titleLists := getTitleList(titleListDoc)
+
+	//取最后一页的内容太少，再取前一页，防止一次更新太多时，会缺少
+	if len(titleLists) < num {
+		secondC := ret[len(ret)-2]
+		titleLists = append(getTitleList(get(url + secondC.Url)))
+	}
+
+	//fmt.Printf("%+v\n", titleLists)
+	return ret
+}
+
 func get(url string) *goquery.Document {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
@@ -32,42 +58,28 @@ func get(url string) *goquery.Document {
 	utfBody, err := iconv.NewReader(res.Body, "gbk", "utf-8")
 	if err != nil {
 		fmt.Printf("%s\n", "Error Encoding")
-		os.Exit(44)
+		os.Exit(-1)
 	}
 	doc, err := goquery.NewDocumentFromReader(utfBody)
 	if err != nil {
 		fmt.Printf("%s", "Error decode body")
-		return nil
+		os.Exit(-1)
 	}
 	return doc
 }
 
-// GetLastTitleList 获取最近更新列表
-func GetLastTitleList(num int) []cont {
-	url := "https://m.biqiuge.com/book_4772/"
-	ret := make([]cont, 10)
-	doc := get(url)
-	fmt.Printf("%v", doc)
-	doc.Find(".books .listpage select option").Each(func(i int, s *goquery.Selection) {
-		values, _ := s.Attr("value")
-		c := cont{Url: values}
-		ret = append(ret, c)
-		//fmt.Printf("%s--%s\n", s.Text(), values)
-	})
-
-	//取最后一页上的所有章节名
-	lastC := ret[len(ret)-1]
-	titleListDoc := get(url + lastC.Url)
-	titleLists := make([]cont, 10)
-	TitleListDoc.Find(".books .book_last").Each(func(i int, s *goquery.Selection) {
+func getTitleList(titleListDoc *goquery.Document) []cont {
+	var titleLists []cont
+	titleListDoc.Find(".books .book_last").Each(func(i int, s *goquery.Selection) {
 		if i == 1 {
 			s.Find("dl dd a").Each(func(ii int, ss *goquery.Selection) {
-				urlStr, _ := ss.Attr("href")
-				t := cont{Url: UrlStr, Title: ss.Text()}
-				titleLists = append(titleLists, t)
+				urlStr, eUrlStr := ss.Attr("href")
+				if eUrlStr {
+					t := cont{Url: urlStr, Title: ss.Text()}
+					titleLists = append(titleLists, t)
+				}
 			})
 		}
 	})
-	fmt.Printf("%+v", titleLists)
-	return ret
+	return titleLists
 }
