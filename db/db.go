2485b2e93256314e43a9f89dd758045e3e1e43d2
moonwwdz <wangy3483@gmail.com>
2018-11-19 21:02:36 +0800

    m

--- db/db.go
+++ db/db.go
@@ -1,33 +1,33 @@
 package db
 
+import "database/sql"
+
 var tableName string
 
-func init(){
+func init() {
 	db, err := sql.Open("sqlite3", "./foo.db")
 	if err != nil {
 		panic(err)
 	}
 }
+
 //SetTable 设置表名的同时，创建表
-func SetTable(tableName string){
+func SetTable(tableName string) {
 	tableName = tableName
 }
 
 //Savecont 保存内容
-func SaveCont(title,cont string) (id int, err error){
-	stmt, err := db.Prepare("insert into " + tableName + "(title,title_md5,cont) values (?,?,?)" )
-	if err != nil{
-		return nil,err
+func SaveCont(title, cont string) (id int, err error) {
+	stmt, err := db.Prepare("insert into " + tableName + "(title,title_md5,cont) values (?,?,?)")
+	if err != nil {
+		return nil, err
 	}
 
-	res, err := stmt.Exec(title,util.GetMd5Hash(title),cont)
-	if err != nil{
-		return nil,err
+	res, err := stmt.Exec(title, util.GetMd5Hash(title), cont)
+	if err != nil {
+		return nil, err
 	}
 
-	id,_ := res.LastInsertId()
-	return id,nil
+	id, _ := res.LastInsertId()
+	return id, nil
 }
-
-
-
